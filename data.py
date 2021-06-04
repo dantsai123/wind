7383635802f27bfc6cb6829ad76e453db0658aca
Oscar Key <oscar.t.key@gmail.com>
2021-06-04 11:45:17 +0100

    Updat PyTorch dataset.

--- data.py
+++ data.py
@@ -10,40 +10,46 @@ DATA_ROOT = "/home/oscar/nobackup/datasets/UKCP18"
 DURATION = 36000
 WIDTH = 82
 HEIGHT = 112
+N_GUST_BUCKETS = 10
+FIELD_NAMES = ["psl", "uas", "vas", "sfcWind", "pr"]
 
 
-class PointwiseDataset(Dataset):
-    def __init__(self, field_names: List[str], training: bool) -> None:
+class UKCP18(Dataset):
+    def __init__(self, training: bool) -> None:
         self._n = DURATION
-        self._d = len(field_names)
+        self._d = len(FIELD_NAMES)
 
-        input_data = [load_field(name)[1].values for name in field_names]
+        print("Loading dataset...")
+        input_data = [load_field(name)[1].values for name in FIELD_NAMES]
+        print("Loaded")
         input_tensors = [torch.tensor(d) for d in input_data]
         inputs = torch.stack(input_tensors, axis=4)
-        inputs = inputs.reshape(DURATION, HEIGHT, WIDTH, self._d)
-        targets = torch.tensor(load_field("wsgsmax")[1].values)
-        targets = targets.reshape(DURATION, HEIGHT, WIDTH)
+        inputs = inputs.reshape(DURATION, HEIGHT, WIDTH, self._d).permute(0, 3, 1, 2)
+        target_reals = torch.tensor(load_field("wsgsmax")[1].values)
+        target_reals = target_reals.reshape(DURATION, HEIGHT, WIDTH)
 
-        train_times = range(0, 34000)
+        bucket_boundaries = torch.linspace(0, target_reals.max(), N_GUST_BUCKETS)
+        targets = torch.bucketize(target_reals, bucket_boundaries)
 
-        self._inputs = inputs
-        self._targets = targets
+        train_end_time = 34000
+        if training:
+            self._inputs = inputs[:train_end_time]
+            self._targets = targets[:train_end_time]
+        else:
+            self._inputs = inputs[train_end_time:]
+            self._targets = targets[train_end_time:]
 
-    def __get_item__(self, i: int) -> Tuple[Tensor, Tensor]:
-        # lat, lon, time, val1, val2, ..., ...
-        # time is just days since the first day
-        # t =
-        inputs = self._inputs.reshape(-1, self._d)[i]
-        targets = self._targets.reshape(-1)[i]
+    def __getitem__(self, i: int) -> Tuple[Tensor, Tensor]:
+        return self._inputs[i], self._targets[i]
 
-    def get_shift_factors(self) -> List[float]:
-        pass
+    def get_shift_factors(self) -> Tensor:
+        return torch.min(self._inputs, dim=0)
 
-    def get_scale_factors(self) -> List[float]:
-        pass
+    def get_scale_factors(self) -> Tensor:
+        return self._inputs.std(0)
 
     def __len__(self) -> int:
-        pass
+        return self._inputs.size(0)
 
 
 def load_field(name):
